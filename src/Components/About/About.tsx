b5a84b71e222220aec18c917da84244edb399d0e
Oscar Hermawan <oscar.hermawan90@gmail.com>
2024-06-21 00:48:56 +0800

    AboutComponent

---
+++ src/Components/About/About.tsx
@@ -0,0 +1,141 @@
+// dependancies
+import { Grid, Typography, Divider, Button, useMediaQuery } from "@mui/material"
+import profile from "./Oscar.jpeg"
+import Resume from "../Home/resume.pdf"
+
+// icons
+import { FaRegFilePdf } from "react-icons/fa"
+
+// file imports
+import useAboutStyles from "./AboutStyles"
+
+const About = () => {
+  // classes and queries
+  const classes = useAboutStyles()
+  const md = useMediaQuery("(max-width: 960px)")
+  const lg = useMediaQuery("(min-width: 960px)")
+
+  return (
+    <div className={classes.background}>
+      <Grid id="about" container>
+        {/* Title */}
+        <Grid className={classes.title} item xs={12}>
+          <br />
+          <Typography variant="h2">About Me</Typography>
+          <Typography variant="h4">- Get To Know Me -</Typography>
+          <br />
+          <br />
+        </Grid>
+        <Grid item xs={1}></Grid>
+        {md && <Grid item xs={1}></Grid>}
+        <Grid item xs={10} md={5}>
+          <div className={classes.picSpacing}>
+            <img
+              alt="Display"
+              src={profile}
+              className={classes.profilePic}
+            ></img>
+          </div>
+          <br />
+          <br />
+          <br />
+        </Grid>
+        {md && <Grid item xs={1}></Grid>}
+
+        {/* Text */}
+        <Grid item xs={10} md={5}>
+          <br />
+          <Typography
+            variant="h6"
+            className={classes.subTitle}
+            display="inline"
+          >
+            My name is Oscar Hermawan and I am a
+          </Typography>
+          <Typography
+            variant="h6"
+            style={{ color: "#175fca" }}
+            className={classes.subTitle}
+            display="inline"
+          >
+            {" "}
+            Software Engineer
+          </Typography>
+          <div style={{ paddingBottom: "2%" }} />
+          <Typography variant="body1">
+            I have experienced work on web and mobile development with various
+            framework / library such as Javascript, React, React Native, Vue,
+            Express JS, NodeJs, Typescript, MySQL, PostgreSQL. I have experience
+            working on Bank / Finance, Video Commerce, Real Estate Commerce,
+            Funding Company. Currently I'm curious and learning about Go
+            Language (Performance), and Python (Data Science). Some on my
+            hobbies are Playing Futsal, Playing Video Games, Trying solve
+            codewars problems, and Travelling with my wife. I have experience
+            working in web and mobile development with various frameworks and
+            libraries such as JavaScript, React, React Native, Vue, Express.js,
+            Node.js, TypeScript, MySQL, and PostgreSQL. I have worked in
+            industries including Banking/Finance, Video Commerce, Real Estate
+            Commerce, and Funding Companies. Currently, I am curious about and
+            learning Go (for performance) and Python (for data science). Some of
+            my hobbies include playing futsal, playing video games, watching
+            movies, solving Codewars problems, and traveling with my wife.
+            <b>
+              Currently, I am seeking a full-time job as a Full Stack Developer.
+            </b>
+          </Typography>
+          <br />
+          <Divider />
+          <br />
+
+          {/* Buttons */}
+          {lg && (
+            <a
+              rel="noreferrer"
+              target="_blank"
+              href={Resume}
+              className={classes.links}
+            >
+              <div>
+                <Button
+                  variant="contained"
+                  size="large"
+                  className={classes.button}
+                  startIcon={<FaRegFilePdf />}
+                >
+                  Resume
+                </Button>
+              </div>
+            </a>
+          )}
+          {md && (
+            <a
+              rel="noreferrer"
+              target="_blank"
+              href={Resume}
+              className={classes.links}
+            >
+              <div>
+                <Button
+                  variant="contained"
+                  size="large"
+                  className={classes.buttonMobile}
+                  startIcon={<FaRegFilePdf />}
+                >
+                  Resume
+                </Button>
+              </div>
+            </a>
+          )}
+        </Grid>
+        {md && <Grid item xs={1}></Grid>}
+
+        {/* Bottom */}
+        <Grid item xs={12}>
+          <div style={{ paddingBottom: "7%" }} />
+        </Grid>
+      </Grid>
+    </div>
+  )
+}
+
+export default About
---
+++ src/Components/About/AboutStyles.ts
@@ -0,0 +1,73 @@
+import { makeStyles } from "@mui/styles"
+import { Theme } from "@mui/material/styles"
+
+const AboutStyles = makeStyles((theme: Theme) => ({
+  title: {
+    textAlign: "center",
+    color: theme.palette.secondary.dark,
+  },
+  background: {
+    backgroundColor: "white",
+  },
+  subTitle: {
+    fontWeight: "600",
+  },
+  subTitleSmall: {
+    fontWeight: "600",
+  },
+  picSpacing: {
+    height: "100%",
+    display: "flex",
+    alignItems: "center",
+  },
+  profilePic: {
+    transition: "0.3s",
+    width: "80%",
+    height: "auto",
+    "&:hover": {
+      transition: "0.3s",
+      boxShadow:
+        "0 6px 12px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+    },
+  },
+  skills: {
+    display: "inline-block",
+    "&:hover": {
+      transition: "0.3s",
+      color: "#b5befa",
+    },
+  },
+  button: {
+    backgroundColor: theme.palette.secondary.dark,
+    borderRadius: 20,
+    color: "#ffffff",
+    marginRight: "2%",
+    textTransform: "none",
+    width: "30%",
+    height: "auto",
+    "&:hover": {
+      transition: "0.3s",
+      color: "#000000",
+      backgroundColor: "#EDF1FA",
+    },
+  },
+  buttonMobile: {
+    backgroundColor: theme.palette.secondary.dark,
+    borderRadius: 20,
+    color: "#ffffff",
+    marginRight: "2%",
+    textTransform: "none",
+    width: "42%",
+    height: "auto",
+    "&:hover": {
+      transition: "0.3s",
+      color: "#000000",
+      backgroundColor: "#EDF1FA",
+    },
+  },
+  links: {
+    textDecoration: "none",
+  },
+}))
+
+export default AboutStyles
