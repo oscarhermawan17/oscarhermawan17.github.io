4a21621b7f932a3bd8566d7f071d2e1f54ed8eed
Oscar Hermawan <oscar.hermawan90@gmail.com>
2024-06-21 16:00:38 +0800

    MainComponent - Adding CSS Reset

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client"
 import App from "./App"
 import { ThemeProvider } from "@mui/material/styles"
 import theme from "./theme"
+import "./main.css"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
